9286a9b1a0d1eca68b499093b4fad60a92effe87
Jordan Schneidler <jordan.schneidler@gmail.com>
2016-11-07 18:40:49 -0600

    Modularized database connection

---
+++ server/db.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+
+var config = require('./config'); // ./config.js
+
+function connect() {
+	mongoose.connect(config.mongo.prod_url, function(err) {
+	  if (err) {
+	    console.error('Could not connect to MongoDB.');
+	    return;
+	  }
+	  console.log('Connected to MongoDB.');
+	});
+
+	return {
+		"test": 2
+	};
+}
+
+module.exports = connect;
