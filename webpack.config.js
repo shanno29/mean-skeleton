2cdbbf770b1dd2b55ef429eb5e9c3b3dfee40289
Jordan Schneidler <jordan.schneidler@gmail.com>
2017-01-29 22:06:33 -0600

    No bower

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
           presets: ['react', 'es2015']
         },
         test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/
+        exclude: /(node_modules)/
       }
     ]
   }
