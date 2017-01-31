0a823612f09b9ef2b987d28ccf3185a6d13553ad
Jordan Schneidler <schne482@uwm.edu>
2017-01-31 17:06:55 -0600

    Added ESLint.

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "extends": "google",
+    "parserOptions": {
+      "ecmaVersion": 6,
+      "sourceType": "module",
+      "ecmaFeatures": {
+        "jsx": true
+      }
+    },
+    "env": {
+      "browser": true,
+      "node": true,
+      "mocha": true,
+      "mongo": true
+    }
+};
